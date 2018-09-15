24f27474362f315f0b6349d35766e84e33bc20ac
luno <lunosnyper@gmail.com>
2018-09-16 00:26:14 +0200

    Added live edits

--- websocket_server.py
+++ websocket_server.py
@@ -2,9 +2,10 @@ from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 from pyA20.gpio import gpio
 from pyA20.gpio import port
 import json
+import time
 
-leftPort = port.PA7
-rightPort = port.PA8
+leftPort = port.PG7
+rightPort = port.PG6
 
 @staticmethod
 def setMotorsClockWise():
@@ -30,19 +31,27 @@ def stopMotors():
 class SimpleEcho(WebSocket):
 
     def handleMessage(self):
-        controller_json = json.loads(self.data)
-        speed = controller_json['speed']
-        direction = controller_json['direction']
-
-        if (speed == 0 & direction == 0):
-            stopMotors()
-        elif (speed == 0 & direction < 0):
-            setMotorsClockWise()
-        elif (speed == 0 & direction > 0):
-            setMotorsCounterClockWise()
-
-        # echo message back to client
+        print("I am in the handle method")
         self.sendMessage(self.data)
+        print("I sent the message")
+        try:
+            controller_json = json.loads(self.data)
+            print("I am loading json")
+            speed = controller_json['speed']
+            direction = controller_json['direction']
+            if (speed == 0 & direction == 0):
+                gpio.output(leftPort, gpio.LOW)
+    	        #gpio.output(rightPort, gpio.LOW)
+            elif (speed == 0 & direction < 0):
+                gpio.output(leftPort, gpio.HIGH)
+                #setMotorsClockWise()
+            elif (speed == 0 & direction > 0):
+                gpio.output(leftPort, gpio.HIGH)
+                #gpio.output(rightPort, gpio.LOW)
+        except:
+            print("something bad happened")
+        # echo message back to client
+        #self.sendMessage(self.data)
 
     def handleConnected(self):
         print(self.address, 'connected')
@@ -51,9 +60,17 @@ class SimpleEcho(WebSocket):
         print(self.address, 'closed')
 
 
-gpio.init()
-gpio.setcfg(port.PA7, gpio.OUTPUT)
-gpio.setcfg(port.PA8, gpio.OUTPUT)
-
 server = SimpleWebSocketServer('', 8000, SimpleEcho)
+gpio.init()
+gpio.setcfg(leftPort, gpio.OUTPUT)
+gpio.setcfg(rightPort, gpio.OUTPUT)
+
+#blink
+#for x in range(0, 4):
+#    gpio.output(leftPort, gpio.HIGH)
+#    time.sleep(2)
+#    gpio.output(leftPort, gpio.LOW)
+#    time.sleep(1)
+#gpio.output(rightPort, gpio.LOW)
+
 server.serveforever()
